616ea1d3d61205dd0677fbcc2390b51aed50d1ef
Shaun <shauncuier@gmail.com>
2026-09-20 06:32:29 +0600

    fix: include top-level links in Paginated::from envelope to match standard resource collection

--- app/Support/Paginated.php
+++ app/Support/Paginated.php
@@ -37,6 +37,12 @@ final class Paginated
     {
         return [
             'data' => array_values(array_map($map, $paginator->items())),
+            'links' => [
+                'first' => $paginator->url(1),
+                'last' => $paginator->url($paginator->lastPage()),
+                'prev' => $paginator->previousPageUrl(),
+                'next' => $paginator->nextPageUrl(),
+            ],
             'meta' => [
                 'current_page' => $paginator->currentPage(),
                 'from' => $paginator->firstItem(),
